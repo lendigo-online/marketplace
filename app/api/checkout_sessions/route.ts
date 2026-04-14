ecba85361340e0a5159f717b6de3f58394dfa576
Prezes <325kaffil@gmail.com>
2026-04-14 14:04:50 +0200

    2.84

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -50,7 +50,7 @@ export async function POST(request: Request) {
             cancel_url: `${process.env.NEXTAUTH_URL}/listings/${listingId}?canceled=1`,
             client_reference_id: user.id,
             mode: "payment",
-            payment_method_types: ["card"],
+            automatic_payment_methods: { enabled: true },
             line_items: [
                 {
                     price_data: {
